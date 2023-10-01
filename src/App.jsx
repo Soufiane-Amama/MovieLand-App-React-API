17e089dc4c24d46dd7bfd517a8cc58b12e69daa4
Soufiane Amama <95310971+Soufiane-Amama@users.noreply.github.com>
2023-10-01 23:21:24 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const api_url = "https://www.omdbapi.com/?i=tt3896198&apikey=3bc1b209";
+const API_URL = "https://www.omdbapi.com/?i=tt3896198&apikey=3bc1b209";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -15,7 +15,7 @@ const App = () => {
   }, []);
 
   const searchMovies = async (title) => {
-    const response = await fetch(`${api_url}&s=${title}`);
+    const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
     setMovies(data.Search);
